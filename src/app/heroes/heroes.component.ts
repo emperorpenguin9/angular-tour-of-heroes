4ccac4fadc53e18baf4aa465160e287bb96b8e4b
emperorpenguin9 <flying.penguin9196@outlook.com>
2024-04-18 16:14:58 -0400

    add message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import {
-  NgIf,
-  NgFor,
-  UpperCasePipe,
-  /* . . . */
-} from '@angular/common';
-import { HEROES } from '../mock-heroes';
+import { NgFor } from '@angular/common';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -20,13 +15,20 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent {
   heroes: Hero[] = [];
   selectedHero?: Hero;
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
   ngOnInit(): void {
     this.getHeroes();
   }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
