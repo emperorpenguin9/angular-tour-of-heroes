bd782a6efe8dc294421d88fa4fd3a67215e280a1
emperorpenguin9 <flying.penguin9196@outlook.com>
2024-04-19 09:14:25 -0400

    Add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  declarations: [AppComponent, MessagesComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HeroesComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
 })
 export class AppModule {}
